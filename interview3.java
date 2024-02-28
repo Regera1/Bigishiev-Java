6b62691bf43e84d24b85007456742a178fbcb501
Dzhapar <dzhapar995>
2024-02-28 20:09:11 +0300

    ver2

--- interview3.java
+++ interview3.java
@@ -4,16 +4,17 @@ public class interview3 {
     // написать программу, которая выведет в консоль все чётные числа.
 
     public static void main(String[] args) {
-    
-        int[] array = new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-        
-        for(int i = 0; i <= array.length; i++)
-        {
-            if(i % 2 == 0)
-        {
-            System.out.println(i);
+
+        int[] array = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+        for (int i = 0; i <= array.length; i++) {
+
+            int num = array[i];
+
+            if (num % 2 == 0) {
+                System.out.println(num);
+            }
         }
-        
-        }
+
     }
 }
